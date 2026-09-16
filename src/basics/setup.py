55e88fa4268eb95c8ced585fbd33695fe1d11265
IVzzVll <ivanvazquezv@icloud.com>
2026-09-16 11:29:53 -0600

    Inicial: agrega ejemplos de LED y potenciometro vistos en clase

---
+++ src/basics/setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages, setup
+
+package_name = 'basics'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='zadrac',
+    maintainer_email='ivanvazquezv@icloud.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+           'velocity_publisher = basics.velocity_publisher:main',
+	   'velocity_subscriber = basics.velocity_subscriber:main',
+	   'velocity_turtle_pub = basics.velocity_turtle_pub:main',
+	   'velocity_turtle_subs = basics.velocity_turtle_subs:main',
+	   'led_blink = basics.led_blink:main',
+	   'serial_bridge = basics.serial_bridge:main',
+           'analog_serial_pub = basics.analog_serial_pub:main',
+	   'analog_subs = basics.analog_subs:main',
+        ],
+    },
+)
